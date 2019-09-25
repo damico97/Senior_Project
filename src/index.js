5393c83aa5d6e617d8c37b3f2ded99f32b9f093a
Joey Damico <damico31097@gmail.com>
2019-09-25 09:54:17 -0400

    Working Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/**
+ * @file index.js
+ * @author Joey Damico
+ * @date September 25, 2019
+ * @summary The main extry point for the program
+ */
 import React from "react";
 import ReactDOM from "react-dom";
 
